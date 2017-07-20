2d17c3fa6c65833ba3dce46bd31a2d2619ed38c6
Maxim Fainberg <maksym.fainberg@mobenga.com>
2017-07-20 13:06:37 +0200

    Put regexep into external file

--- podcast-man/src/podcast_man/core.clj
+++ podcast-man/src/podcast_man/core.clj
@@ -1,23 +1,10 @@
 (ns podcast-man.core
   (:gen-class))
+  
+;(:require [clojure.pprint :as pprint]))
 
-(def patterns [
-  #"^6[23].*"
-  #"^3[.][0-9].*" 
-  #"^atp.*"
-  #"^CTOcast.*"
-  #"^hanselminutes.*"
-  #"^devzen.*"
-  #"^functionalgeekery.*"
-  #"^SE-Radio.*"
-  #"^rt_podcast.*"
-  #"^haskellcast.*"
-  #"^partially_derivative.*"
-  #"^razbor.*"
-  #"^sri_en_swetoday.*"
-  #"^Turing.*"
-])
-(def ps (map #(-> {:p %, :i %2}) patterns (range)))
+(defn make-patterns [pattern-list] 
+  (map #(-> {:p %, :i %2}) pattern-list (range)))
 
 (defn read-files [dir]
   (->> dir 
@@ -26,8 +13,9 @@
       (drop 1)
       (map #(.getName %))))
 
-(defn get-pattern-index [fileName] 
-  (or (some #(when (re-matches (:p %) fileName) (:i %)) ps) :no-pattern))  
+(defn get-pattern-index [patterns] 
+  (fn [file-name] 
+    (or (some #(when (re-matches (:p %) file-name) (:i %)) patterns) :no-pattern)))  
 
 (defn get-element [pattern-index files]
   (map #(-> {:name %, 
@@ -36,19 +24,25 @@
        files 
        (range))) 
    
-(defn f [dir]
+(defn main [dir pattern-list]
   (let [files (read-files dir)
-        map-pattern-index-to-files (group-by get-pattern-index files)
+        patterns (make-patterns pattern-list)
+        map-pattern-index-to-files (group-by (get-pattern-index patterns) files)
         map-clean (dissoc map-pattern-index-to-files :no-pattern)
         elements (reduce-kv (fn [acc k v] (concat acc (get-element k v))) [] map-clean)
         sorted-elements (sort-by (juxt :value :index ) elements)
         rename-list (map #(-> {:old (str dir "/" (:name %)), :new (format "%s/%03d_%s" dir %2 (:name %))}) sorted-elements (range))
-         ]
+    ]
     (do 
       (println "Files beyond templates:" (:no-pattern map-pattern-index-to-files))
       (println (map #(.renameTo (clojure.java.io/file (:old %)) (clojure.java.io/file (:new %))) rename-list)))))
   
+(defn get-pattern-list []
+  (-> "./podcasts"
+      (slurp)
+      (clojure.string/split #"\n")
+      (#(map re-pattern %)))) 
 
 (defn -main
   [& args]
-  (f (first args)))
+  (main (first args) (get-pattern-list)))
